63e6f03786a91755eff6662615253a6f94cf7e47
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-09-01 06:46:57 -0300

    ViaCEP API Request

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -3,30 +3,44 @@ import './styles.css';
 import ResultCard from 'components/ResultCard';
 import React from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 
 type FormData = {
-  cep: string,
-  test: string
-}
+  cep: string;
+};
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+};
 
 const CepSearch = () => {
+  const [address, setAddress] = useState<Address>();
 
   const [formData, setFormData] = useState<FormData>({
     cep: '',
-    test: ''
   });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
     const value = event.target.value;
-    setFormData({ ...formData, [name]:value })
-  }
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // previne/evita que, nesse caso, o formulário
-                            //  seja enviado assim que clicado o botão
-    console.log(formData);
-  }
+    event.preventDefault();
+
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json/`)
+      .then((response) => {
+        setAddress(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
+  };
 
   return (
     <div className="cep-search-container">
@@ -42,23 +56,17 @@ const CepSearch = () => {
               placeholder="CEP (somente números)"
               onChange={handleChange}
             />
-            <input
-              type="text"
-              name="test"
-              value={formData.test}
-              className="search-input"
-              placeholder="Teste"
-              onChange={handleChange}
-            />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
             </button>
           </div>
         </form>
-
-        <ResultCard title="Logradouro" description="Lalala" />
-        <ResultCard title="Número" description="234" />
-
+        {address && (
+          <>
+            <ResultCard title="Logradouro" description={address?.logradouro} />
+            <ResultCard title="Localidade" description={address?.localidade} />
+          </>
+        )}
       </div>
     </div>
   );
